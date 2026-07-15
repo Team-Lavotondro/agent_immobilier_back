35399fe15b1e27e9873529083ddc3da83636636a
ruta mickael <rutamickael619@gmail.com>
2026-07-16 02:08:42 +0300

    feat : Modification et suppression utilisateur

--- src/utilisateur/controller/user.controller.ts
+++ src/utilisateur/controller/user.controller.ts
@@ -8,21 +8,36 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UserService } from '../services/user.service';
+import { UpdateUtilisateurDto } from '../dto/update-utilisateur.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('list')
+  @Get('')
   async getAllUser() {
     const res = this.userService.getAllUtil();
     return res;
   }
-  
+
   @Get(':id')
-   async getDetailUser(@Param('id') id:string) {
+  async getDetailUser(@Param('id') id: string) {
     const res = this.userService.getDetailUtil(id);
     return res;
   }
 
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    const res = await this.userService.deleteUser(id);
+    return res;
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() user: UpdateUtilisateurDto,
+  ) {
+    const res = await this.userService.updateUser(id, user);
+    return res;
+  }
 }
--- src/utilisateur/dto/update-utilisateur.dto.ts
+++ src/utilisateur/dto/update-utilisateur.dto.ts
@@ -1,4 +1,40 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUtilisateurDto } from './create-utilisateur.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateUtilisateurDto extends PartialType(CreateUtilisateurDto) {}
+export class UpdateUtilisateurDto {
+
+  @ApiPropertyOptional({
+    description: "Nom de l'utilisateur",
+    example: "Rakoto",
+  })
+  @IsOptional()
+  @IsString()
+  nom_util?: string;
+
+
+  @ApiPropertyOptional({
+    description: "Numéro de téléphone de l'utilisateur",
+    example: "0340000000",
+  })
+  @IsOptional()
+  @IsString()
+  num_tel_util?: string;
+
+
+  @ApiPropertyOptional({
+    description: "Nouveau mot de passe de l'utilisateur",
+    example: "NouveauMotDePasse123",
+  })
+  @IsOptional()
+  @IsString()
+  mdp_util?: string;
+
+
+  @ApiPropertyOptional({
+    description: "Activation de l'espace de vente",
+    example: true,
+  })
+  @IsOptional()
+  @IsBoolean()
+  espace_vente?: boolean;
+}
